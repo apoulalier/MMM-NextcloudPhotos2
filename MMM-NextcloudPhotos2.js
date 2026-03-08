84874f46213162a82cd827b3617c305fe0bf170a
apoulalier <a.poulalier@gmail.com>
2026-03-08 20:39:51 +0100

    small test

--- MMM-NextcloudPhotos2.js
+++ MMM-NextcloudPhotos2.js
@@ -20,7 +20,7 @@ Module.register("MMM-NextcloudPhotos2", {
     maxWidth: 1920,                  // Max image width in pixels
     maxHeight: 1080,                 // Max image height in pixels
     imageQuality: 80,               // JPEG quality (1-100)
-    albums: ["Anniversaire 1 an", "Danemark 2024"],
+    albums: ["TESTAX"],
     timeFormat: "DD/MM/YYYY HH:mm",
   },
 
